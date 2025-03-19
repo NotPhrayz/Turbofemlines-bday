f369b77cd49ebca689edf5fd8ea625bd2f1e326b
NotPhrayz <notghosty32@gmail.com>
2025-03-19 15:13:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ const animateElement = (element, animation, duration = 300) => {
   
   // Audio System
   const voiceMessage = new Howl({
-    src: ["birthday.mp3"],
+    src: ["audio/birthday.mp3"],
     html5: true,
     volume: 0.8,
     onplay: () => {
